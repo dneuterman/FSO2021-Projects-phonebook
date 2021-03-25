c26d09c8b620312b07919726cf379b8847704b97
Donald Neuterman <dneuterman@gmail.com>
2021-03-25 02:46:10 -0400

    part 2b phonebook exercises complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Person = ({ person }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <li>{person.name}: {person.number}</li>
+  )
 }
 
-export default App;
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', id: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', id: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', id: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ showFilter, setShowFilter ] = useState(persons)
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject ={
+      name: newName,
+      number: newNumber,
+      id: newName
+    }
+
+    for (const person of persons) {
+      if (person.name.toLowerCase() === nameObject.name.toLowerCase()) {
+        return (
+          window.alert(`${nameObject.name} is already in the phonebook.`),
+          setNewName(''),
+          setNewNumber('')
+        )
+      }
+    }
+
+    setPersons(persons.concat(nameObject))
+    setShowFilter(persons.concat(nameObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value
+
+    if (filterValue === '') {
+      setShowFilter(persons)
+    } else {
+      setShowFilter(persons.filter(person => person.name.toLowerCase().includes(filterValue)))
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter: <input onChange={handleFilterChange} />
+      </div>
+      <h2>Add a New</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {showFilter.map( person =>
+        <Person key={person.id} person={person} />
+      )}
+    </div>
+  )
+}
+
+export default App
